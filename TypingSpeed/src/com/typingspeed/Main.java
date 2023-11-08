f933ea5e603fccf6e550806a3261c8be0bcff789
Deepak Kumaravelu <67898647+deepakkumaravelu@users.noreply.github.com>
2023-11-08 21:53:57 +0530

    typespeed java code uploaded

---
+++ TypingSpeed/src/com/typingspeed/Main.java
@@ -0,0 +1,22 @@
+package com.typingspeed;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// Main method is the entry point of the program and contains the implementation
+		// of the menu and starts the typing test.
+		Scanner sc = new Scanner(System.in);
+
+		// text is store in Text.java file
+		String text = "The quick brown fox jumps over the lazy dog";
+
+		System.out.println("Select mode:");
+		System.out.println("1. Type full text");
+		System.out.println("2. Type in one minute");
+		int mode = sc.nextInt();
+
+		TypingTest test = new TypingTest(text, mode);
+		test.startTest();
+		sc.close();
+	}
+}
---
+++ TypingSpeed/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HARI DEEPAK
+ *
+ */
+module TypingSpeed {
+}
